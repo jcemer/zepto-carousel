75b922e4c14ceb9bffcc1edb7946c0c627e3e4cc
Jcemer <jeancarloemer@gmail.com>
2012-08-06 07:38:43 -0300

    css obj

--- zepto-carousel.js
+++ zepto-carousel.js
@@ -1,10 +1,51 @@
 ;(function($) {
     'use strict';
 
+    // thanks to http://www.mobify.com/
+    var CSS = {
+        cache: {},
+        has: {},
+        prefixes: ['Webkit', 'Moz', 'O', 'ms', ''],
+
+        getProperty: function(name) {
+            var div, property;
+            if (typeof CSS.cache[name] !== undefined) {
+                return CSS.cache[name];
+            }
+
+            div  = document.createElement('div').style;
+            for (var i = 0; i < CSS.prefixes.length; ++i) {
+                if (div[CSS.prefixes[i] + name] !== undefined) {
+                    return CSS.cache[name] = CSS.prefixes[i] + name;
+                }
+            }
+        }
+    };
+
+    CSS.has = {
+        'transform': !! (CSS.getProperty('Transform')),
+        'transform3d': !! (window.WebKitCSSMatrix && 'm11' in new WebKitCSSMatrix()) 
+    };
+
+    CSS.translateX = function(element, delta) {
+        var property = CSS.getProperty('Transform');
+        if (typeof delta === 'number') {
+            delta = delta + 'px';
+        }
+        if (CSS.has.transform3d) {
+            element.style[property] = 'translate3d(' + delta  + ', 0, 0)';
+        } else if (CSS.has.transform) {
+            element.style[property] = 'translate(' + delta  + ', 0)';
+        } else {
+            element.style.left = delta;
+        }
+    };
+
+
     var defaults = {
         prev: '.prev',
         next: '.next'
-    }
+    };
 
     $.fn.carousel = function (opts) {
         var settings = $.extend({}, defaults, opts),
@@ -29,7 +70,7 @@
             move = {
                 reset: function () {
                     current.removeClass('moving');
-                    current[0].style['-webkit-transform'] = 'translate3d(0, 0, 0)';
+                    CSS.translateX(current[0], 0);
                 },
                 next: function () {
                     var element = current.next();
@@ -64,8 +105,7 @@
             var x = event.touches[0].pageX;
             current.addClass('moving');
             function animate(event) {
-                //event.preventDefault();
-                current[0].style['-webkit-transform'] = 'translate3d(' + (event.touches[0].pageX - x) + 'px, 0, 0)';
+                CSS.translateX(current[0], event.touches[0].pageX - x);
             };
             function stop(event) {
                 doc.off('touchmove', animate);
@@ -86,6 +126,6 @@
         } else if (current.is(':last-child')) {
             btn.next.addClass('disable');
         }
-    }
+    };
 
 })(Zepto);
